aabe2e51b7e95d032ab6d5dd6a742f2d1ae0737c
fan-of-stl <abhishek1168953@gmail.com>
2025-03-13 03:29:10 +0530

    Filter moldecule is added along with lottie loader for showing comming soon clip .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+import React from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import comingSoonAnimation from "./assets/coming-soon.json";
 import "./App.css";
-import SearchBar from "./components/moldecules/SearchBar";
+import Grid from "@mui/material/Grid2";
+import AText from "./components/atoms/AText";
 
 function App() {
   return (
     <div className="App">
-      <SearchBar
-        placeholder={"Search"}
-        onSearch={() => {
-          console.log("Searched");
-        }}
-      />
+      <div className="loader-container">
+        <Grid container sx={{ alignItems: "center" }}>
+          <Grid size={{ sm: 12 }}>
+            <Player
+              autoplay
+              loop
+              src={comingSoonAnimation}
+              style={{ height: "80vh", width: "80vw" }}
+            />
+          </Grid>
+          <Grid
+            size={{ sm: 12 }}
+            sx={{ alignItems: "center" }}
+            textAlign={"center"}
+          >
+            <AText variant="h4">Home Page Coming Soon...</AText>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
---
+++ src/components/moldecules/FilterBar.jsx
@@ -0,0 +1,71 @@
+import { MenuItem } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import React, { useState } from "react";
+import AInput from "../atoms/AInput";
+import AIcon from "../atoms/AIcon";
+import ACard from "../atoms/ACard";
+import SearchIcon from "@mui/icons-material/Search";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import AButton from "../atoms/AButton";
+
+const FilterBar = ({ categories, onFilterChange }) => {
+  const [filters, setFilters] = useState({
+    search: "",
+    category: "",
+  });
+
+  const handleChange = (field, value) => {
+    setFilters((prev) => {
+      const updatedFilters = { ...prev, [field]: value };
+      onFilterChange(updatedFilters);
+      return updatedFilters;
+    });
+  };
+
+  return (
+    <ACard>
+      <Grid container flex={"row"} spacing={2} alignItems="center">
+        <Grid size={{ xs: 4, sm: 4 }}>
+          <AInput
+            label="Search..."
+            fullWidth
+            value={filters.search}
+            onChange={(e) => handleChange("search", e.target.value)}
+            InputProps={{
+              endAdornment: <AIcon name={SearchIcon} />,
+            }}
+          />
+        </Grid>
+
+        <Grid size={{ xs: 4, sm: 4 }}>
+          <AInput
+            select
+            fullWidth
+            label="Category"
+            value={filters.category}
+            onChange={(e) => handleChange("category", e.target.value)}
+          >
+            {categories?.map((cat) => (
+              <MenuItem key={cat} value={cat}>
+                {cat}
+              </MenuItem>
+            ))}
+          </AInput>
+        </Grid>
+
+        <Grid size={{ xs: 2, sm: 2 }}>
+          <AButton
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={() => onFilterChange(filters)}
+          >
+            <AIcon name={FilterListIcon} /> Filter
+          </AButton>
+        </Grid>
+      </Grid>
+    </ACard>
+  );
+};
+
+export default FilterBar;
